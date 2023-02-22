8ff94098f837f777e7ecebb589921a27f524e514
kristian-wittrup <kw@easv.dk>
2023-02-22 11:01:39 +0100

    edit part

--- template-parts/page/content.php
+++ template-parts/page/content.php
@@ -24,5 +24,31 @@
     ?>
   </div>
 
+  <?php if( get_edit_post_link() ) : ?>
+
+  <footer class="entry-footer"> 
+
+    <?php 
+
+    edit_post_link(
+      sprintf(
+        wp_kses(
+          __( 'Edit <span class="screen-reader-text">%s</span>' ),
+          array(
+            'span' => array(
+              'class' => array()
+            )
+          )
+            ),
+            get_the_title()
+          ),
+          '<span class="edit-link">',
+          '</span>'
+    );
+
+    ?>
+
+  </footer>
+    <?php endif; ?>
 
 </article>
