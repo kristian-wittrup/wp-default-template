c8bd5e1ee0bc870338d98b1d1753bdda28f21dda
kristian-wittrup <kw@easv.dk>
2023-03-01 10:34:33 +0100

    added own template + css + archive

---
+++ aboutus.php
@@ -0,0 +1,8 @@
+<?php 
+/**
+ * Template Name: About Us
+ */
+get_header();
+?>
+
+<div id="box1"> Box1 </div>
---
+++ template-parts/post/content-none.php
@@ -0,0 +1,8 @@
+<?php 
+/**
+ * Content none
+  */
+  get_header()
+?>
+
+<h1>No content - searchbar soon</h1>
