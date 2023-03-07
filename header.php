12809ee7f57de545b41278000b51c743f2731189
kristian-wittrup <kw@easv.dk>
2023-03-07 13:01:57 +0100

    .
    .

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=$, initial-scale=1.0">
   <title>Document</title>
+  
   <?php wp_head(); ?>
   </head>
   
