f36e1ba0e4e191339a8ae4e1a7b3f689ff4149d3
kristian-wittrup <kw@easv.dk>
2023-03-08 14:25:06 +0100

    the_content() for aboutus-template + css

--- aboutus.php
+++ aboutus.php
@@ -7,8 +7,12 @@ get_header();
 <div id="primary" class="content-area">
   <main id="main" class="site-area">
 
-  
-    <div id="box1"> All ma content </div>
+
+    <div id="box1">
+    <?php 
+      the_content(  );    
+    ?>
+    </div>
 
   </main>
 </div>
